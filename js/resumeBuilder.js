c5cb2b73b486cbb091b1a8a6d25ae8e6d43dd5b9
Tan <haitanle88@gmail.com>
2015-10-27 21:47:32 -0400

    add contact information to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,17 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Tan Le");
+$("#header").append(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Software Engineer");
+$("#header").append(formattedRole);
+
+var phone = HTMLmobile.replace("%data%", "704-451-2956"); 
+$("#header").append(phone); 
+
+var email = HTMLemail.replace("%data%", "haitansle@gmail.com"); 
+$("#header").append(email); 
+
+var github = HTMLgithub.replace("%data%", "haitanle");
+$("#header").append(github); 
+
+var message = HTMLwelcomeMsg.replace("%data%", "Message goes here"); 
+$("#header").append(message); 
